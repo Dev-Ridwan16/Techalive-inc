2ba1f8a872a8bffcbf6b21088e91b077ed3e6517
Dev-Ridwan16 <devridwan1@gmail.com>
2025-07-06 19:37:26 +0100

    feat: add favicon to html head

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,29 +1,28 @@
-import type { Metadata } from "next";
-import { Outfit } from "next/font/google";
-import "./globals.css";
+import type { Metadata } from 'next'
+import { Outfit } from 'next/font/google'
+import './globals.css'
 
 const outfit = Outfit({
-  variable: "--font-outfit-sans",
-  subsets: ["latin"],
-});
+  variable: '--font-outfit-sans',
+  subsets: ['latin'],
+})
 
-// const geistMono = Geist_Mono({
-//   variable: "--font-geist-mono",
-//   subsets: ["latin"],
-// });
+export const metadata: Metadata = {
+  title: 'Your App Title',
+  description: 'Your App Description',
+  icons: {
+    icon: 'https://i.imgur.com/UKGl5Qk.png', // Or '/favicon.png' if local
+  },
+}
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode;
+  children: React.ReactNode
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={`${outfit.variable}`}
-      >
-        {children}
-      </body>
+    <html lang='en'>
+      <body className={outfit.variable}>{children}</body>
     </html>
-  );
+  )
 }
